f6780a1e7d01616f715bc6ea8cc686b31ec10653
CheChenItza-lab <rodriguez.hernandez.23187@itsmante.edu.mx>
2026-05-11 00:25:40 -0600

    Prueba de API reciba mensaje estoy dentro

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller('auth')
 export class AuthController{
-
+constructor(private authService: AuthService){
+ 
+    }
+    @Post('signup')
+    signup(){
+        return 'Estoy registrado'
+    }
+    @Post('signin')
+    signin(){
+        return 'Ya estoy dentro'
+    }
 }
